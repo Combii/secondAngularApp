0c7796b750991c658eaa1226486b16c663dc2c78
Combii <davidstovle@gmail.com>
2020-02-12 11:03:08 +0100

    t

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is a simply a test',
+      'https://i.pinimg.com/236x/76/92/79/76927939ad6134c1b5b0fa472803ca4b--crochet-hats-kids-events.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
